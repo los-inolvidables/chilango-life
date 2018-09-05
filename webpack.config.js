b01a86c76649e87b463bb4fc6a08efac2152da82
JessicaShalomG <jsphxusa072@gmail.com>
2018-09-05 10:33:59 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/react-client/src');
+var DIST_DIR = path.join(__dirname, '/react-client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module : {
+    rules : [
+      {
+        test : /\.jsx?/,
+        exclude : /node_modules|packages/,
+        include : SRC_DIR,
+        loader : 'babel-loader'
+      }
+    ]
+  }
+};
