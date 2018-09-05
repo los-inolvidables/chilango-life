2f036780cbc9b09536115162fbd4729e781392d2
Usuario <usuario@softbank220001000064.bbtec.net>
2018-09-05 16:13:00 -0500

    created the mysql database file

---
+++ database/index.js
@@ -0,0 +1,22 @@
+var mysql = requiered('mysql');
+
+var connection = mysql.createConnection({
+
+  host : 'localhost',
+  user : 'root',
+  password : 'password',
+  database : 'mike'
+
+});
+
+var selectAll = function(callback) {
+  conection.query('SELECT * FROM forum' function(err,results,fields){
+    if(err) {
+      callback(err, null);
+    }else{
+      callback(null,results);
+    }
+  } );
+};
+
+module.exports.selectAll = selectAll;
