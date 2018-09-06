039d80c27c7ca5ff23020aec43d976748edcc2e6
Erickdegnte <erickdegante3003@gmail.com>
2018-09-06 12:19:55 -0500

    carousel ok

--- react-client/src/components/Carousel.js
+++ react-client/src/components/Carousel.js
@@ -11,3 +11,48 @@ class Carousel extends React.Component {
       active: 0
     };
   }
+
+  render() {
+    return (
+      //set up for stylesheet
+     <div>
+        <Coverflow
+          width={960}
+          height={350}
+          displayQuantityOfSide={2}
+          navigation={true}
+          enableHeading={false}
+          active={this.state.active}
+        >
+        //set up for the buttons
+          <div
+            onClick={() => fn()}
+            onKeyDown={() => fn()}
+            role="menuitem"
+            tabIndex="0"
+          >
+//url for the Carousel image
+          </div>
+          <img src='https://i.imgur.com/Ok6XIk0.jpg'alt='valle de bravo'/>
+          <img src='https://i.imgur.com/AEmUarJ.jpg'alt='zocalo'/>
+          <img src='https://i.imgur.com/1wObCoB.jpg'alt='teotihuacan'/>
+          <img src='https://i.imgur.com/VdlHK2n.jpg'alt='valle'/>
+          <img src='https://i.imgur.com/qijGsWm.jpg'alt='bellas artes'/>
+          <img src='https://i.imgur.com/jAZiQy5.png'alt='papalote'/>
+
+
+        </Coverflow>
+      </div>
+
+    );
+  }
+
+  _handleClick() {
+    var num = Math.floor((Math.random() * 10) + 1);
+    this.setState({
+      active: num
+    });
+  }
+};
+
+export default Carousel;
