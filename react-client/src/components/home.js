0d4d7ca3cadc467e065f7f89e5afd67f09512de1
JessicaShalomG <jsphxusa072@gmail.com>
2018-09-06 21:13:44 -0500

    unstaged

--- react-client/src/components/home.js
+++ react-client/src/components/home.js
@@ -2,8 +2,8 @@ import React , { Component } from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Bar from "./bar.js";
-import TypeWriteR from "./typeWriter.jsx";
-import Carousel from "./components/Carousel.js";
+import TypeWriteR from "./typeWriter.jsx"
+
 
 class Home extends React.Component{
   render(){
@@ -12,9 +12,8 @@ class Home extends React.Component{
         <div className="main">
           <h1>Chilango Life</h1>
         </div>
-        
           <TypeWriteR />
-          <Carousel />
+          
           </div>
 
 
