9efaf8687d5ba34f14b961ecf27576e3690645d9
mike <angelrosillo2018@gmail.com>
2018-09-07 18:18:51 -0500

    shortDescriptionAdded

--- react-client/src/components/close/piramides.js
+++ react-client/src/components/close/piramides.js
@@ -4,10 +4,13 @@ import { player } from "video-react";
 const Piramides =()=>{
   return(
     <div className= "route">
+
     <iframe width="560" height="315"
      src="https://www.youtube.com/embed/2JdcM-jCTks"
-     frameborder="0" allow="autoplay; encrypted-media"
-     allowfullscreen></iframe>
+     frameBorder="0" allow="autoplay; encrypted-media"
+     allowFullscreen></iframe>
+
+     <p>Mexico un lugar hermoso</p>
     </div>
   );
 };
