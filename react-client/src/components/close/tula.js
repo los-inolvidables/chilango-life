fc8c788e5033263900297cba9d5381cb8c36e3b4
mike <angelrosillo2018@gmail.com>
2018-09-07 20:49:39 -0500

    description added to tula

--- react-client/src/components/close/tula.js
+++ react-client/src/components/close/tula.js
@@ -6,7 +6,9 @@ const Tula =()=>{
     <iframe width="560" height="315"
      src="https://www.youtube.com/embed/DHId0-yJFz0?autoplay=1&controls=0&&showinfo=0"
      frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    <p> Tula is Working
+    <p> Tula de Allende (Otomi: Mämeni) is a town and one of the 84 municipalities of Hidalgo in central-eastern Mexico. The municipality covers an area of 305.8 km² (118.07 sq mi), and as of 2010, the municipality had a total population of 103,919. The municipality includes numerous smaller outlying towns, the largest of which are El Llano, San Marcos, and San Miguel Vindho.
+    </p>
+    <p> It is a regional economic center and one of Mexico’s fastest growing cities. However, it is best known as the home of the Tula archeological site, noted for its Atlantean figures. Its built-up area (or metro) made up of Atotonilco de Tula, Atitalaquia, Tlaxcoapan municipalities was home to 188,659 inhabitants at the 2010 census.
     </p>
     </div>
   );
