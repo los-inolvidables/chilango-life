c5438603c7090361761f1496a8770e786587a169
mike <angelrosillo2018@gmail.com>
2018-09-07 20:55:35 -0500

    description added to valle

--- react-client/src/components/close/valle.js
+++ react-client/src/components/close/valle.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Valle =()=>{
   return(
     <div className= "route">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/4JGqq9tRGwc?autoplay=1&controls=0&&showinfo=0"
+    frameBorder="0" allow="autoplay; encrypted-media"
+     allowFullScreen></iframe>
     <p> Valle is Working
     </p>
     </div>
