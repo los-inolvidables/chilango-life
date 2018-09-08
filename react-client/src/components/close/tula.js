5ba105c8cdc2e66aeed265ee8f62c3279b9694d7
mike <angelrosillo2018@gmail.com>
2018-09-07 20:34:21 -0500

    video tula rendering

--- react-client/src/components/close/tula.js
+++ react-client/src/components/close/tula.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Tula =()=>{
   return(
     <div className= "route">
+    <iframe width="560" height="315"
+     src="https://www.youtube.com/embed/DHId0-yJFz0?autoplay=1&controls=0&&showinfo=0"
+     frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     <p> Tula is Working
     </p>
     </div>
