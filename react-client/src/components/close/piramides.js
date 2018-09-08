3963b575638b116a82204b13c65d327c04aa3116
mike <angelrosillo2018@gmail.com>
2018-09-07 19:13:29 -0500

    description added to file piramides

--- react-client/src/components/close/piramides.js
+++ react-client/src/components/close/piramides.js
@@ -5,13 +5,16 @@ const Piramides =()=>{
   return(
     <div className= "route">
 
-    <iframe width="560" height="315"
-     src="https://www.youtube.com/embed/2JdcM-jCTks"
-     frameBorder="0" allow="autoplay; encrypted-media"
-     allowFullscreen></iframe>
+      <iframe width="560" height="315"
+      src="https://www.youtube.com/embed/2JdcM-jCTks"
+      frameBorder="0" allow="autoplay; encrypted-media"
+      allowFullScreen></iframe>
+     <p>Teotihuacán o Teotihuacan (en náhuatl: Teōtihuācan, ‘‘lugar donde los hombres se convierten en dioses ’;1​ ‘lugar donde se hicieron los dioses ’; ‘ciudad de los dioses’’) 2​, o también Teo uacan (en náhuatl: 'Ciudad del sol')3​ es el nombre que se da a la que fue una de las mayores ciudades prehispánicas de Mesoamérica. El topónimo es de origen náhuatl y fue empleado por los mexicas para identificar a esta ciudad construida por una civilización anterior a ellos y que ya se encontraba en ruinas cuando los mexicas la vieron por primera vez. A la fecha se desconoce el nombre que le daban sus habitantes originales. Los restos de la ciudad se encuentran al noreste del valle de México, en los municipios de Teotihuacán y San Martín de las Pirámides (estado de México), aproximadamente a 78 kilómetros de distancia del centro de la Ciudad de México. La zona de monumentos arqueológicos fue declarada Patrimonio de la Humanidad por la Unesco en 1987.</p>
+     <p>Teotihuacán ha sido motivo de interés para las sociedades posteriores al declive de la cultura teotihuacana en Mesoamérica. Sus ruinas han sido exploradas desde la época prehispánica, entre otros, por los toltecas y los mexicas. El descubrimiento de objetos teotihuacanos en los yacimientos arqueológicos de Tula y el Templo Mayor de México-Tenochtitlan así lo confirma. En la mitología nahua posclásica, la ciudad aparece como el escenario de mitos fundamentales como la leyenda de los Soles de los mexicas.</p>
 
-     <p>Mexico un lugar hermoso</p>
+     <p>Actualmente, los restos de Teotihuacán constituyen la zona de monumentos arqueológicos con mayor afluencia de turistas en México, por encima de Chichén Itzá, El Tajín y Monte Albán. Las excavaciones arqueológicas en Teotihuacán continúan hasta nuestros días, y han dado como resultado un paulatino incremento en la calidad y cantidad del conocimiento que se tiene sobre esta ciudad.</p>
     </div>
+
   );
 };
 
