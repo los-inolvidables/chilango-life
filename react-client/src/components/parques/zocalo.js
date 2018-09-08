48db1a4cb04651c36f11d5b535c6689edcc0f0f8
mike <angelrosillo2018@gmail.com>
2018-09-08 10:29:02 -0500

    video cetered

--- react-client/src/components/parques/zocalo.js
+++ react-client/src/components/parques/zocalo.js
@@ -5,9 +5,9 @@ const Zocalo =()=>{
   return(
     <div>
       <div>
-        <iframe className= "videos" width="560" height="315"
+        <center><iframe className= "videos" width="560" height="315"
          src="https://www.youtube.com/embed/wils1-Ln0tk?autoplay=1&controls=0&&showinfo=0"
-         frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+         frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></center>
       </div>
       <div className= "route">
         <p> Zocalo works bitches
